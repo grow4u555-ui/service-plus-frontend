fe6e4eef2de3541b5d2f97a5df565c1f0b9de571
grow4u555-ui <grow4u555@gmail.com>
2025-08-18 14:53:22 +0200

    Update and rename App.jsx to App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
